ee089fe96a2ee73096df1185b080141502c85fa5
ffozilbek <fozilbekfayzullayev05@gmail.com>
2025-02-17 14:22:39 +0500

    add features images and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Homepage, Companies } from "./components";
+import { Navbar, Homepage, Companies, Features } from "./components";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <main>
         <Homepage />
         <Companies />
+        <Features />
       </main>
     </>
   );
